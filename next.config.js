12b08ca47c418457251e276e705aac3a32d272bb
张洲铭-MBA <1142783160@qq.com>
2023-02-26 16:53:04 +0800

    feat: i18n

--- next.config.js
+++ next.config.js
@@ -1,6 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ['en', 'zh'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'en',
+    // This is a list of locale domains and the default locale they
+    // should handle (these are only required when setting up domain routing)
+    // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
+    // domains: [
+    //   {
+    //     domain: 'example.com',
+    //     defaultLocale: 'en-US',
+    //   },
+    //   {
+    //     domain: 'example.nl',
+    //     defaultLocale: 'nl-NL',
+    //   },
+    //   {
+    //     domain: 'example.fr',
+    //     defaultLocale: 'fr',
+    //     // an optional http field can also be used to test
+    //     // locale domains locally with http instead of https
+    //     http: true,
+    //   },
+    // ],
+  },
 }
 
 module.exports = nextConfig
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import {NextIntlProvider} from 'next-intl';
+ 
+export default function App({Component, pageProps}) {
+  return (
+    <NextIntlProvider messages={pageProps.messages}>
+      <Component {...pageProps} />
+    </NextIntlProvider>
+  );
 }
