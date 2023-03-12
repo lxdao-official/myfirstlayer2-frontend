3d4b0115be4ba474ab8dd5866b4d3156f532c81e
huauhua <194542159@qq.com>
2023-03-12 12:19:09 +0800

    feat: mdx nav

--- components/MyComponent.js
+++ components/MyComponent.js
@@ -1,3 +1,34 @@
+import {
+  Link,
+  Card,
+  Box,
+  Typography,
+  Grid,
+  CardContent,
+  CardActions,
+} from '@mui/material';
+
+const IMG = [
+  '/team/bruce.jpg',
+  '/team/muxin.png',
+]
+
 export default function MyComponent() {
-  return <p>import cmp</p>;
+
+  return (
+    <Box
+      padding={{ xs: 2, md: 4 }}
+      marginTop={{ xs: 4, md: 6 }}
+      position="relative"
+    >
+      <Box display="flex" justifyContent="space-between">
+        <Box>
+          <img src={IMG[0]} width={100} height={100}></img>
+        </Box>
+        {/* <Box>
+          12345678901234567890
+        </Box> */}
+      </Box>
+    </Box>
+  );
 }
