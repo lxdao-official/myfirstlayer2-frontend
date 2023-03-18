f873eaa3d4ec26db9e235f08d2f547ebc20597b9
huauhua <194542159@qq.com>
2023-03-18 21:11:25 +0800

    feat: add bottom nav component

--- contents/Index.js
+++ contents/Index.js
@@ -10,48 +10,50 @@ import {
 } from '@mui/material';
 import Container from '../components/Container';
 // import Directory from "./Directory";
-import {MobileDirectory, PcDirectory } from './Directory';
+import { PcDirectory } from './Directory';
 import TabChapter from './TabChapter';
-
+import BottomNav from './BottomNav';
 export default function Content(props) {
   const { md } = props;
   
   return (
-    <Container
-      marginTop={4}
-    >
-      <Box
-            display="flex"
-            justifyContent="space-between"
+    <>
+      <Container
+        marginTop={4}
       >
         <Box
-          marginRight={{
-            xs: 0,
-            sm: 2,
-          }}
+          display="flex"
+          justifyContent="space-between"
         >
           <Box
-            sx={{
-              backgroundColor: '#ECECEC',
-              // width: 920,
+            marginRight={{
+              xs: 0,
+              sm: 2,
             }}
-            // width
-            borderRadius={2}
-            padding={{
-              xs: 2,
-              sm: 8,
-            }}
-          >{md}</Box>
-          <TabChapter marginTop={{xs: '20px', sm: '160px'}}></TabChapter>
+          >
+            <Box
+              sx={{
+                backgroundColor: '#ECECEC',
+                // width: 920,
+              }}
+              // width
+              borderRadius={2}
+              padding={{
+                xs: 2,
+                sm: 8,
+              }}
+            >{md}</Box>
+            <TabChapter marginTop={{xs: '20px', sm: '160px'}}></TabChapter>
 
-        </Box>
-        <Hidden smUp>
-         <MobileDirectory></MobileDirectory>
-       </Hidden>
+          </Box>
         <Hidden smDown>
          <PcDirectory></PcDirectory>
        </Hidden>
       </Box>
     </Container>
+    <Hidden smUp>
+      <BottomNav />
+    </Hidden>
+    </>
   )
 }
