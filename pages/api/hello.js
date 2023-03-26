2ba53049051fab47d784450f35bb9e4674346270
huauhua <194542159@qq.com>
2023-03-26 17:50:55 +0800

    fix: build ci err

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -5,7 +5,14 @@ export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' });
 }
 
-export default function getFiles(req, res) {
-  const files = fs.readdirSync('.//mdx/zh/MyFirst-Layer2_Content');
-  res.status(200).json({ files });
-}
+// export default function getFiles(req, res) {
+//   const files = fs.readdirSync('./mdx/zh/MyFirst-Layer2_Content');
+//   res.status(200).json({ files });
+// }
+
+// export default function writeFiles(req, res) {
+//   const files = fs.readFileSync('./test.js');
+//   res.status(200).json({ files });
+// }
+
+
