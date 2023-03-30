716a694ab16b865656bc6f7a1c3d14e3d07bf671
张洲铭-MBA <1142783160@qq.com>
2023-03-30 23:48:55 +0800

    fix: theme.palette.mode to theme.palette?.mode

--- contents/Progress.js
+++ contents/Progress.js
@@ -1,32 +1,35 @@
 import { useContext } from 'react';
-import { Box, Typography, CircularProgress, LinearProgress } from '@mui/material';
+
+import {
+  Box,
+  CircularProgress,
+  LinearProgress,
+  Typography,
+} from '@mui/material';
 import { makeStyles, withStyles } from '@mui/styles';
 
 import { ReadContext } from './context.js';
-import {
-  setStorage,
-  getStorage,
-  removeStorage,
-} from './storage.js'
+import { getStorage, removeStorage, setStorage } from './storage.js';
 
 const BorderLinearProgress = withStyles((theme) => ({
   root: {
     height: 10,
     borderRadius: 5,
-    color: theme.palette.mode === 'dark' ? '#fff' : '#000',
+    color: theme.palette?.mode === 'dark' ? '#fff' : '#000',
   },
   colorPrimary: {
-    backgroundColor: theme.palette.mode === 'dark' ? '#4E4E4E' : '#AEAEAE', 
+    backgroundColor: theme.palette?.mode === 'dark' ? '#4E4E4E' : '#AEAEAE',
   },
   bar: {
     borderRadius: 5,
-    backgroundColor: theme.palette.mode === 'dark' ? '#ffffff' : 'background: #000000',
-  }
+    backgroundColor:
+      theme.palette?.mode === 'dark' ? '#ffffff' : 'background: #000000',
+  },
 }))(LinearProgress);
 
 const useStyles = makeStyles({
   root: {
-    flexGrow: 1
+    flexGrow: 1,
   },
 });
 
@@ -41,9 +44,13 @@ export default function Progress() {
 
   return (
     <div className={classes.root}>
-      <BorderLinearProgress variant="determinate" value={(unRead / counter) * 100} />
-      <Typography style={{transform: `translateX(${x}%)`}}>{ ((unRead / counter) * 100).toFixed(2)}%</Typography>
+      <BorderLinearProgress
+        variant="determinate"
+        value={(unRead / counter) * 100}
+      />
+      <Typography style={{ transform: `translateX(${x}%)` }}>
+        {((unRead / counter) * 100).toFixed(2)}%
+      </Typography>
     </div>
   );
 }
-
