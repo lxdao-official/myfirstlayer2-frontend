bd855746bd5e833b478c6c89a0a6063dcb14714a
张洲铭-MBA <1142783160@qq.com>
2023-04-02 21:31:39 +0800

    feat: mint nft, alert error

--- components/ZksyncSwap.js
+++ components/ZksyncSwap.js
@@ -1,15 +1,62 @@
-import { useTranslations } from 'next-intl';
 import { useEffect, useRef, useState } from 'react';
-import { useAccount } from 'wagmi';
+import { useAccount, useContractWrite, useWaitForTransaction } from 'wagmi';
 
-import { Box, Typography, useMediaQuery, useTheme } from '@mui/material';
+import { Box, Button } from '@mui/material';
+import { Stack } from '@mui/system';
+
+import abi from '../abi.json';
+import { uploadPNG } from '../common/ipfs';
+import showMessage from '../components/showMessage';
 
 export default function ZksyncSwap({ children, title }) {
   const { address } = useAccount();
   const canvasRef = useRef(null);
-  const imgRef = useRef(null);
   const [imgLoaded, setImgLoaded] = useState(false);
+  const [mintLoading, setMintLoading] = useState(false);
   const [modifiedImgSrc, setModifiedImgSrc] = useState('');
+
+  const { data, writeAsync } = useContractWrite({
+    address: '0x43c4Ebf956F7804596c333B209Ff246a476594DA',
+    abi: abi,
+    functionName: 'mint',
+    mode: 'recklesslyUnprepared',
+  });
+  const { isLoading, isSuccess } = useWaitForTransaction({
+    hash: data?.hash,
+  });
+  const handleMint = async () => {
+    try {
+      setMintLoading(true);
+      const cid = await uploadPNG(modifiedImgSrc);
+      const data = btoa(
+        JSON.stringify({
+          imgurl: `ipfs://${cid}`,
+        })
+      );
+      console.log(error);
+      console.log(writeAsync);
+      const res = await writeAsync?.({
+        recklesslySetUnpreparedArgs: ['data:application/json;base64,' + data],
+      });
+      console.log(res);
+      setMintLoading(false);
+    } catch (err) {
+      showMessage({
+        type: 'error',
+        title: 'Estimate Fail',
+        body: 'You may have already minted or other reason.',
+      });
+      setMintLoading(false);
+    }
+  };
+  useEffect(() => {
+    if (isSuccess) {
+      showMessage({
+        type: 'success',
+        title: `Mint Successfully`,
+      });
+    }
+  }, [isSuccess]);
 
   useEffect(() => {
     // 获取 canvas 元素和上下文对象
@@ -36,11 +83,11 @@ export default function ZksyncSwap({ children, title }) {
 
       // 在 canvas 上执行绘制操作
       ctx.fillStyle = '#E9E9E9';
-      ctx.font = '96px Open Sans';
-      ctx.fillText(`timestamp ${new Date().getTime()}`, 402, 1952);
+      ctx.font = '24px Open Sans';
+      ctx.fillText(`timestamp ${new Date().getTime()}`, 100, 488);
       ctx.fillStyle = '#6C6C6C';
-      ctx.font = '40px Open Sans';
-      ctx.fillText(address, 530, 2033);
+      ctx.font = '10px Open Sans';
+      ctx.fillText(address, 132, 508);
 
       // 将修改后的图片转换为 base64 格式
       const modifiedImgSrc = canvas.toDataURL('image/png');
@@ -49,13 +96,19 @@ export default function ZksyncSwap({ children, title }) {
   }, [imgLoaded]);
 
   return (
-    <Box display="flex" justifyContent="center" marginTop={2}>
-      <canvas
-        ref={canvasRef}
-        width={'2048px'}
-        height={'2427px'}
-        style={{ zoom: 0.25 }}
-      ></canvas>
-    </Box>
+    <Stack gap={7} marginTop={2} alignItems="center">
+      <Box>
+        <canvas ref={canvasRef} width={512} height={606}></canvas>
+      </Box>
+
+      <Button
+        fullWidth
+        variant="contained"
+        disabled={isLoading | mintLoading}
+        onClick={handleMint}
+      >
+        {isLoading | mintLoading ? 'Minting...' : 'Mint'}
+      </Button>
+    </Stack>
   );
 }
