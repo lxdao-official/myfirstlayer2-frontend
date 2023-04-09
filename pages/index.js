28501ae88028ce7e0078ca838bc838eccc8e2945
Jacky <sjyanxin@126.com>
2023-04-09 19:15:40 +0800

    feat: add diploma

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,28 @@
 import fs from 'fs';
 import { LXDAOIntroduction } from 'lxdao-ui';
-import { GetStaticPropsContext } from 'next';
-import { useLocale, useTranslations } from 'next-intl';
 import { MDXRemote } from 'next-mdx-remote';
 import { serialize } from 'next-mdx-remote/serialize';
-import Head from 'next/head';
 import path from 'path';
 import React from 'react';
 
 import { Box } from '@mui/material';
 
-import Container from '../components/Container';
-import MintBadge from '../components/MintBadge';
-import ZksyncSwap from '../components/ZksyncSwap';
-import CompressText from '../components/animation/CompressText';
+import Diploma from '../components/Diploma';
 import Content from '../contents/Index';
-import { getStorage, removeStorage, setStorage } from '../contents/storage';
 import Main from '../layouts/Main';
 import SectionFooter from '../sections/SectionFooter';
 import SectionMyFirstProject from '../sections/SectionMyFirstProject';
 import SectionSponsors from '../sections/SectionSponsors';
 import SectionTeam from '../sections/SectionTeam';
 import SectionTop from '../sections/SectionTop';
-import Directory from '/components/Directory';
 import { formatDirectory, getDocBySlug } from '/utils';
-import dynamic from "next/dynamic";
-const ImpossibleTriangle = dynamic(
-  () => import('../components/ImpossibleTriangle'),
-  { ssr: false }
-)
 
-export default function Index({ content, meta, directory }) {
-  const t = useTranslations('Index');
-  const locale = useLocale();
-  const components = {};
+export default function Index({ content, directory }) {
   return (
     <Main>
-      <ImpossibleTriangle />
+      {/* <Diploma /> */}
       <SectionTop />
       <Content md={<MDXRemote {...content} file={directory} />} />
-      <CompressText />
-      {/* <CompressAni /> */}
-      <ZksyncSwap />
-      <MintBadge />
       <SectionMyFirstProject />
       <SectionSponsors />
       <SectionTeam />
@@ -55,13 +35,10 @@ export default function Index({ content, meta, directory }) {
 }
 
 export async function getStaticProps({ locale }) {
-  const directoryPath = path.join(
-    process.cwd(),
-    '/mdx/zh/MyFirst-Layer2_Content'
-  );
+  const directoryPath = path.join(process.cwd(), '/mdx/zh/MyFirst-Layer2_Content');
   const files = fs.readdirSync(directoryPath);
   const fileNames = files.map((file) => file);
-  const directory = formatDirectory(fileNames);
+  const directory = formatDirectory(fileNames).filter((item) => item.text !== 'README' && item.text !== 'SUMMARY');
 
   const { content, meta } = getDocBySlug(directory[0]?.text, locale);
   const mdxSource = await serialize(content);
