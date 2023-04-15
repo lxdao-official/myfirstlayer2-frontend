99058a6cbc4ce9f0d6819c4102a382ff98b6b722
张洲铭-MBA <1142783160@qq.com>
2023-04-15 13:35:11 +0800

    fix head

--- sections/SectionTop.js
+++ sections/SectionTop.js
@@ -9,35 +9,35 @@ export default function SectionTop({ content, meta }) {
   const t = useTranslations('Index');
 
   return (
-    <Container id="top-section">
-      <Typography
-        sx={{
-          textTransform: 'uppercase',
-          fontSize: { xs: '70px', md: '90px' },
-          fontWeight: '800',
-          whiteSpace: 'pre-wrap',
-          lineHeight: { xs: '70px', md: '100px' },
-        }}
-      >
-        {t('title')}
-      </Typography>
-      <Box
-        component="img"
-        src="/images/top-background.png"
-        my={'30px'}
-        width={'100%'}
-        borderRadius={'16px'}
-      />
-      <Typography
-        sx={{
-          fontWeight: '400',
-          color: 'text.primary',
-          fontSize: '20px',
-          whiteSpace: 'pre-line',
-        }}
-      >
-        {t('description')}
-      </Typography>
-    </Container>
+    <Box sx={{ backgroundImage: 'url(/assets/top-day.png)', backgroundPosition: 'center' }} width="100%" color="#ffffff" height={'860px'}>
+      <Box sx={{ backgroundPosition: 'center', background: 'linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0.79) 42.71%, rgba(0, 0, 0, 0) 100%)' }} width="100%" color="#ffffff" height={'860px'}>
+        <Container id="top-section" margin="0 auto" paddingY={'100px'}>
+          <Typography
+            sx={{
+              textTransform: 'uppercase',
+              fontSize: { xs: '70px', md: '90px' },
+              fontWeight: '800',
+              whiteSpace: 'pre-wrap',
+              lineHeight: { xs: '70px', md: '100px' },
+            }}
+          >
+            {t('title')}
+          </Typography>
+
+          <Typography
+            sx={{
+              mt: '50px',
+              fontWeight: '400',
+              color: 'text.primary',
+              fontSize: '20px',
+              color: 'white',
+              whiteSpace: 'pre-line',
+            }}
+          >
+            {t('description')}
+          </Typography>
+        </Container>
+      </Box>
+    </Box>
   );
 }
