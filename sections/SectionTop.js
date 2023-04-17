bf4a937146d3512f10f152030f86b3302cad4c0b
张洲铭-MBA <1142783160@qq.com>
2023-04-17 16:25:02 +0800

    add mobile

--- sections/SectionTop.js
+++ sections/SectionTop.js
@@ -9,16 +9,16 @@ export default function SectionTop({ content, meta }) {
   const t = useTranslations('Index');
 
   return (
-    <Box sx={{ backgroundImage: 'url(/assets/top-day.png)', backgroundPosition: 'center' }} width="100%" color="#ffffff" height={'860px'}>
-      <Box sx={{ backgroundPosition: 'center', background: 'linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0.79) 42.71%, rgba(0, 0, 0, 0) 100%)' }} width="100%" color="#ffffff" height={'860px'}>
-        <Container id="top-section" margin="0 auto" paddingY={'100px'}>
+    <Box sx={{ backgroundImage: 'url(/assets/top-day.png)', backgroundPosition: 'center', backgroundSize: 'cover' }} width="100%" color="#ffffff" height={{ xs: '390px', md: '860px' }}>
+      <Box sx={{ backgroundPosition: 'center', background: 'linear-gradient(180deg, #000000 0%, rgba(0, 0, 0, 0.79) 42.71%, rgba(0, 0, 0, 0) 100%)' }} width="100%" color="#ffffff" height={{ xs: '390px', md: '860px' }}>
+        <Container id="top-section" margin="0 auto" paddingTop={{ xs: '20px', md: '100px' }}>
           <Typography
             sx={{
               textTransform: 'uppercase',
-              fontSize: { xs: '70px', md: '90px' },
+              fontSize: { xs: '36px', md: '90px' },
               fontWeight: '800',
               whiteSpace: 'pre-wrap',
-              lineHeight: { xs: '70px', md: '100px' },
+              lineHeight: { xs: '40px', md: '100px' },
             }}
           >
             {t('title')}
@@ -26,10 +26,11 @@ export default function SectionTop({ content, meta }) {
 
           <Typography
             sx={{
-              mt: '50px',
+              mt: { xs: '20px', md: '50px' },
               fontWeight: '400',
               color: 'text.primary',
-              fontSize: '20px',
+              fontSize: { xs: '10px', md: '20px' },
+              lineHeight: { xs: '16px', md: '26px' },
               color: 'white',
               whiteSpace: 'pre-line',
             }}
