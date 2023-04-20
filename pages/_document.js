60a7641bba4c2e101c4e87abc6ebe8c9ed38daec
张洲铭-MBA <1142783160@qq.com>
2023-04-21 00:31:04 +0800

    fix: change des

--- pages/_document.js
+++ pages/_document.js
@@ -8,12 +8,20 @@ export default function Document() {
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link rel="icon" href="/icons/favicon.png" />
         <title>My First Layer2</title>
-        <meta name="description" content="MyFirstLayer2 MyFirstLayer2 MyFirstLayer2" />
+        <meta
+          name="description"
+          content="MyFirstLayer2 is an education project for newbies to learn and interact with Layer2. As you can see from the name, our users are beginners curious about the knowledge and projects on Layer2. We will use funny and easy-to-understand diagrams and animation to explain Layer2 to newbies. "
+        />
         <meta property="og:locale" content="en_US" />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="/icons/favicon.png" />
         <meta property="og:title" content={'MyFirstLayer2'} />
-        <meta property="og:description" content={'MyFirstLayer2 MyFirstLayer2 MyFirstLayer2'} />
+        <meta
+          property="og:description"
+          content={
+            'MyFirstLayer2 is an education project for newbies to learn and interact with Layer2. As you can see from the name, our users are beginners curious about the knowledge and projects on Layer2. We will use funny and easy-to-understand diagrams and animation to explain Layer2 to newbies. '
+          }
+        />
         <meta property="og:url" content="https://lxdao.io/" />
       </Head>
       <base target="_blank"></base>
