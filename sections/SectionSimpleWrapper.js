dab36f334b9d258a042784bba9b70b31946e4f33
Jacky <sjyanxin@126.com>
2023-04-23 00:39:05 +0800

    fix: change my first project style

--- sections/SectionSimpleWrapper.js
+++ sections/SectionSimpleWrapper.js
@@ -2,17 +2,11 @@ import React from 'react';
 
 import { Box, Typography } from '@mui/material';
 
-export default function SectionSimpleWrapper({ id, title, children }) {
+export default function SectionSimpleWrapper({ id, title, children, childOverflow = 'hidden' }) {
   const ref = React.useRef();
 
   return (
-    <Box
-      marginTop={{ xs: 6.875, sm: 6.875, md: 15 }}
-      position="relative"
-      className="section"
-      id={`section/${id}`}
-      ref={ref}
-    >
+    <Box marginTop={{ xs: 6.875, sm: 6.875, md: 15 }} position="relative" className="section" id={`section/${id}`} ref={ref}>
       <Box>
         <Box display="flex" justifyContent="center">
           <Typography
@@ -29,11 +23,7 @@ export default function SectionSimpleWrapper({ id, title, children }) {
             {title}
           </Typography>
         </Box>
-        <Box
-          overflow="hidden"
-          maxWidth={{ sm: 720, md: 960, lg: 1200 }}
-          margin="0 auto"
-        >
+        <Box overflow={childOverflow} maxWidth={{ sm: 720, md: 960, lg: 1200 }} margin="0 auto">
           {children}
         </Box>
       </Box>
