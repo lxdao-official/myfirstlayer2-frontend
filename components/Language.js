df18b3cccbcee35bf27bf355ae662efad0c38996
Jacky <jackysense8@gmail.com>
2023-05-03 14:59:55 +0800

    Change header and banner style

--- components/Language.js
+++ components/Language.js
@@ -47,7 +47,7 @@ const Language = ({ color }) => {
         <Box aria-controls={open ? 'language-menu' : undefined} aria-haspopup="true" aria-expanded={open ? 'true' : undefined} sx={{ cursor: 'pointer' }} onClick={handleClick}>
           <Box display="flex" alignItems="center">
             {!smallScreen && <Earth color={color} />}
-            <Typography marginLeft={smallScreen ? 0 : '5px'} marginRight={smallScreen ? 0 : '5px'} fontWeight={400} color="text.primary" fontSize={'12px'} width={{ xs: 'auto', md: '50px' }} textAlign="center">
+            <Typography marginLeft={smallScreen ? 0 : '5px'} marginRight={smallScreen ? 0 : '5px'} fontWeight={400} color={color} fontSize={'12px'} width={{ xs: 'auto', md: '50px' }} textAlign="center">
               {text[locale][smallScreen ? 'sx' : 'md']}
             </Typography>
             <Arrow color={color} style={{ rotate: open && '180deg' }} />
--- layouts/Main/index.js
+++ layouts/Main/index.js
@@ -1,79 +1,130 @@
-import { useTranslations } from 'next-intl';
-import { useRouter } from 'next/router';
-import { useContext, useEffect, useState } from 'react';
+import { useTranslations } from "next-intl";
+import { useRouter } from "next/router";
+import { useContext, useEffect, useState } from "react";
 
-import { Box, Divider, Link, Menu, MenuItem, Stack, Typography, useMediaQuery, useTheme } from '@mui/material';
-import { display } from '@mui/system';
+import {
+  Box,
+  Divider,
+  Link,
+  Stack,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
 
-import Container from '../../components/Container';
-import LXDAOLogo from '../../components/LXDAOLogo';
-import Language from '../../components/Language';
-import MFL2 from '../../components/svg/MFL2';
-import Question from '../../components/svg/Question';
-import Theme from '../../components/svg/Theme';
-import { ColorModeContext } from '../../pages/_app';
-import SectionTop from '../../sections/SectionTop';
+import Container from "../../components/Container";
+import LXDAOLogo from "../../components/LXDAOLogo";
+import Language from "../../components/Language";
+import MFL2 from "../../components/svg/MFL2";
+import Question from "../../components/svg/Question";
 
-const Main = ({ children, colorInvert = false, bgcolor = 'transparent' }) => {
+import SectionTop from "../../sections/SectionTop";
+import { MFL2ConnectButton } from "../../components/MFL2ConnectButton";
+
+const Main = ({ children, colorInvert = false, bgcolor = "transparent" }) => {
   const router = useRouter();
 
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
-  const t = useTranslations('Main');
+  const t = useTranslations("Main");
   const theme = useTheme();
 
-  const smallScreen = useMediaQuery(theme.breakpoints.down('md'));
+  const smallScreen = useMediaQuery(theme.breakpoints.down("md"));
 
   useEffect(() => {
-    const font = new FontFace('sucaijishikangkangti', 'url(/font/sucaijishikangkangti-2-webfont.woff2)');
+    const font = new FontFace(
+      "sucaijishikangkangti",
+      "url(/font/sucaijishikangkangti-2-webfont.woff2)"
+    );
     document.fonts.add(font);
     font.load();
   }, []);
 
   return (
     <Box>
-      <Box id="fixed-header" bgcolor={'header.main'} paddingX="50px" position="fixed" zIndex={100} width={'100vw'} top={0}>
+      <Box
+        id="fixed-header"
+        bgcolor={"header.main"}
+        paddingX="50px"
+        zIndex={100}
+        width={"100vw"}
+        top={0}
+      >
         {smallScreen ? (
-          <Container display="flex" flexDirection="column" alignItems="center" padding={0}>
-            <Stack width="100%" height="45px" paddingLeft="24px" paddingRight="12px" direction="row" justifyContent="space-between">
-              <Box display="flex" component="a" href="/" title="theFront" flexDirection="row" alignItems="center">
-                <MFL2 width="82px" color={theme.palette.text.primary} />
+          <Container
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            padding={0}
+          >
+            <Stack
+              width="100%"
+              height="45px"
+              paddingLeft="24px"
+              paddingRight="12px"
+              direction="row"
+              justifyContent="space-between"
+            >
+              <Box
+                display="flex"
+                component="a"
+                href="/"
+                title="theFront"
+                flexDirection="row"
+                alignItems="center"
+              >
+                <MFL2 width="82px" color={theme.palette.primary.contrastText} />
                 <Divider
                   orientation="vertical"
                   sx={{
-                    borderColor: theme.palette.text.primary,
-                    height: '16px',
-                    marginInline: '15px',
+                    borderColor: theme.palette.primary.contrastText,
+                    height: "16px",
+                    marginInline: "15px",
                   }}
                 />
-                <LXDAOLogo width={79} height={21} color={theme.palette.text.primary} />
+                <LXDAOLogo
+                  width={79}
+                  height={21}
+                  color={theme.palette.primary.contrastText}
+                />
               </Box>
 
-              <Box sx={{ display: 'flex', alignItems: 'center' }} zIndex={10}>
-                {/* <Theme width={18} height={18} color={theme.palette.text.primary} style={{ cursor: 'pointer' }} onClick={colorMode.toggleColorMode} /> */}
-                <Question width={19} height={19} style={{ margin: '11px' }} color={theme.palette.text.primary} />
-                <Language color={theme.palette.text.primary} />
+              <Box sx={{ display: "flex", alignItems: "center" }} zIndex={10}>
+                {/* <Theme width={18} height={18} color={theme.palette.primary.contrastText} style={{ cursor: 'pointer' }} onClick={colorMode.toggleColorMode} /> */}
+                <Question
+                  width={19}
+                  height={19}
+                  style={{ margin: "11px" }}
+                  color={theme.palette.primary.contrastText}
+                />
+                <Language color={theme.palette.primary.contrastText} />
               </Box>
             </Stack>
-            <Box width="100%" height={'36px'} justifyContent="space-evenly" alignItems="center" display="flex">
+            <Box
+              width="100%"
+              height={"36px"}
+              justifyContent="space-evenly"
+              alignItems="center"
+              display="flex"
+            >
               <Box
                 sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
                 }}
               >
                 <Typography
                   sx={{
-                    cursor: 'pointer',
-                    fontSize: '13px',
-                    fontWeight: '700',
+                    cursor: "pointer",
+                    fontSize: "13px",
+                    fontWeight: "700",
                   }}
                 >
                   <Link
                     sx={{
-                      textDecoration: 'none',
-                      color: theme.palette.text.primary,
+                      textDecoration: "none",
+                      color: theme.palette.primary.contrastText,
                     }}
                     href="#introduce"
                     target="_self"
@@ -84,22 +135,22 @@ const Main = ({ children, colorInvert = false, bgcolor = 'transparent' }) => {
               </Box>
               <Box
                 sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
                 }}
               >
                 <Typography
                   sx={{
-                    cursor: 'pointer',
-                    fontSize: '13px',
-                    fontWeight: '700',
+                    cursor: "pointer",
+                    fontSize: "13px",
+                    fontWeight: "700",
                   }}
                 >
                   <Link
                     sx={{
-                      textDecoration: 'none',
-                      color: theme.palette.text.primary,
+                      textDecoration: "none",
+                      color: theme.palette.primary.contrastText,
                     }}
                     href="#content"
                     target="_self"
@@ -110,22 +161,22 @@ const Main = ({ children, colorInvert = false, bgcolor = 'transparent' }) => {
               </Box>
               <Box
                 sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
                 }}
               >
                 <Typography
                   sx={{
-                    cursor: 'pointer',
-                    fontSize: '13px',
-                    fontWeight: '700',
+                    cursor: "pointer",
+                    fontSize: "13px",
+                    fontWeight: "700",
                   }}
                 >
                   <Link
                     sx={{
-                      textDecoration: 'none',
-                      color: theme.palette.text.primary,
+                      textDecoration: "none",
+                      color: theme.palette.primary.contrastText,
                     }}
                     href="#oinus"
                     target="_self"
@@ -137,38 +188,58 @@ const Main = ({ children, colorInvert = false, bgcolor = 'transparent' }) => {
             </Box>
           </Container>
         ) : (
-          <Container maxWidth="100%" paddingX={0} paddingY={1} display="flex" flexDirection="row" alignItems="center" justifyContent="space-between" height="60px">
-            <Box display="flex" component="a" href="/" title="theFront" flexDirection="row" alignItems="center">
-              <MFL2 color={theme.palette.text.primary} />
+          <Container
+            maxWidth="100%"
+            paddingX={0}
+            paddingY={1}
+            display="flex"
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="space-between"
+            height="60px"
+          >
+            <Box
+              display="flex"
+              component="a"
+              href="/"
+              title="theFront"
+              flexDirection="row"
+              alignItems="center"
+            >
+              <MFL2 color={theme.palette.primary.contrastText} />
               <Divider
                 orientation="vertical"
                 sx={{
-                  borderColor: theme.palette.text.primary,
-                  height: '24px',
-                  marginInline: '20px',
+                  borderColor: theme.palette.primary.contrastText,
+                  height: "24px",
+                  marginInline: "20px",
                 }}
               />
-              <LXDAOLogo width={121} height={32} color={theme.palette.text.primary} />
+              <LXDAOLogo
+                width={121}
+                height={32}
+                color={theme.palette.primary.contrastText}
+              />
             </Box>
             <Box gap={4} display="flex" alignItems="center">
               <Box
                 sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
                 }}
               >
                 <Typography
                   sx={{
-                    fontSize: '15px',
-                    fontWeight: '400',
+                    fontSize: "15px",
+                    fontWeight: "400",
                   }}
                 >
                   <Link
                     sx={{
-                      textDecoration: 'none',
-                      color: theme.palette.text.primary,
-                      cursor: 'pointer',
+                      textDecoration: "none",
+                      color: theme.palette.primary.contrastText,
+                      cursor: "pointer",
                     }}
                     href="#introduce"
                     target="_self"
@@ -179,23 +250,23 @@ const Main = ({ children, colorInvert = false, bgcolor = 'transparent' }) => {
               </Box>
               <Box
                 sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
                 }}
               >
                 <Typography
                   sx={{
-                    cursor: 'pointer',
-                    fontSize: '15px',
-                    fontWeight: '400',
+                    cursor: "pointer",
+                    fontSize: "15px",
+                    fontWeight: "400",
                   }}
                 >
                   <Link
                     sx={{
-                      textDecoration: 'none',
-                      cursor: 'pointer',
-                      color: theme.palette.text.primary,
+                      textDecoration: "none",
+                      cursor: "pointer",
+                      color: theme.palette.primary.contrastText,
                     }}
                     href="#content"
                     target="_self"
@@ -206,22 +277,22 @@ const Main = ({ children, colorInvert = false, bgcolor = 'transparent' }) => {
               </Box>
               <Box
                 sx={{
-                  display: 'flex',
-                  flexDirection: 'column',
-                  alignItems: 'center',
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
                 }}
               >
                 <Typography
                   sx={{
-                    fontSize: '15px',
-                    fontWeight: '400',
+                    fontSize: "15px",
+                    fontWeight: "400",
                   }}
                 >
                   <Link
                     sx={{
-                      textDecoration: 'none',
-                      cursor: 'pointer',
-                      color: theme.palette.text.primary,
+                      textDecoration: "none",
+                      cursor: "pointer",
+                      color: theme.palette.primary.contrastText,
                     }}
                     href="#joinus"
                     target="_self"
@@ -231,16 +302,27 @@ const Main = ({ children, colorInvert = false, bgcolor = 'transparent' }) => {
                 </Typography>
               </Box>
             </Box>
-            <Box sx={{ display: 'flex', alignItems: 'center' }} zIndex={10}>
-              {/* <Theme color={theme.palette.text.primary} style={{ cursor: 'pointer' }} onClick={colorMode.toggleColorMode} /> */}
-              <Question color={theme.palette.text.primary} style={{ margin: '12.5px' }} />
-              <Language color={theme.palette.text.primary} />
+            <Box sx={{ display: "flex", alignItems: "center" }} zIndex={10}>
+              {/* <Theme color={theme.palette.primary.contrastText} style={{ cursor: 'pointer' }} onClick={colorMode.toggleColorMode} /> */}
+
+              <MFL2ConnectButton />
+
+              <Question
+                color={theme.palette.primary.contrastText}
+                style={{ margin: "12.5px", marginLeft: "21px" }}
+              />
+              <Language color={theme.palette.primary.contrastText} />
             </Box>
           </Container>
         )}
       </Box>
-      <SectionTop width={'100vw'} />
-      <Box bgcolor="bodyBg.main" component="main" id="main" paddingTop={{ sx: '80px', md: '120px' }}>
+      <SectionTop width={"100vw"} />
+      <Box
+        bgcolor="bodyBg.main"
+        component="main"
+        id="main"
+        paddingTop={{ sx: "80px", md: "120px" }}
+      >
         {children}
       </Box>
     </Box>
