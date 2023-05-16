906d9bb35296f6416754fdbe2369627855231e18
张洲铭-MBA <1142783160@qq.com>
2023-05-16 14:24:57 +0800

    fix: my first project

--- sections/SectionMyFirstProject.js
+++ sections/SectionMyFirstProject.js
@@ -81,7 +81,6 @@ function MyFirstCard(props) {
           title={props.title}
           sx={{
             width: '100%',
-            height: '177px',
             borderRadius: '15px',
             filter: props.theme.palette?.mode === 'dark' ? 'brightness(0.7)' : 'none',
           }}
@@ -126,8 +125,8 @@ export default function SectionMyFirstProject() {
       {
         projectName: 'My First NFT',
         holder: '',
-        image: '/project/myfirstnft.svg',
-        bigImage: '/project/myfirstnftCard.svg',
+        image: '/project/myfirstnft.png',
+        bigImage: '/project/myfirstnftCard.png',
         description: 'MyFirstNFT 是一个针对 Web3 新手的非营利性教学项目。 在学习 Web3、NFT 的潜在价值和安全原则的同时获得免费的 NFT。',
         url: 'https://myfirstnft.info/',
       },
@@ -144,16 +143,16 @@ export default function SectionMyFirstProject() {
       {
         projectName: 'My First NFT',
         holder: '',
-        image: '/project/myfirstnft.svg',
-        bigImage: '/project/myfirstnftCard.svg',
+        image: '/project/myfirstnft.png',
+        bigImage: '/project/myfirstnftCard.png',
         description: 'MyFirstNFT is a non-profit instructional project for Web3 newbies. Get a FREE NFT while learning about Web3, underlying values of NFT, and security principles.',
         url: 'https://myfirstnft.info/',
       },
       {
         projectName: 'EIPs Fun',
         holder: '',
-        image: '/project/myfirstnft.svg',
-        bigImage: '/project/myfirstnftCard.svg',
+        image: '/project/eipsfun.png',
+        bigImage: '/project/eipsfunCard.png',
         description: 'EIPs Fun is a project for making EIPs fun and easy to be adopted by buidlers and advancing EIP ecosystem development.',
         url: 'https://eips.fun/',
       },
