6b74088d784d021845e49e3c2df55680f94a04e6
huauhua <194542159@qq.com>
2023-05-23 00:17:59 +0800

    feat: component en

--- components/EditChapter.js
+++ components/EditChapter.js
@@ -1,14 +1,17 @@
 import { Box } from '@mui/material';
+import { useTranslations } from 'next-intl';
 
 export default function EditChapter(props) {
   const { url = ''} = props;
+  const t = useTranslations('EditChapter');
+
   return (
     <Box sx={{
       textAlign: 'right',
       marginTop: '20px',
       fontWeight: 700,
     }}>
-      <a href={url}>【编辑文档】</a>
+      <a href={url}>{t('Edit-a-document')}</a>
     </Box>
   )
 }
