924c6123c588859848269e200c9582ff3d2a416a
huauhua <194542159@qq.com>
2023-05-22 22:59:30 +0800

    feat: en content

--- pages/api/getFile/[name].js
+++ pages/api/getFile/[name].js
@@ -8,9 +8,10 @@ import { formatDirectory, getDocBySlug } from '../../../utils';
 export default async function handler(req, res) {
   let { name } = req.query;
 
+  let locale = req.headers['referer'].includes('/zh') ? 'zh' : 'en';
   console.log('req md name->', name);
 
-  const { content, meta } = getDocBySlug(name);
+  const { content, meta } = getDocBySlug(name, locale);
   const mdxSource = await serialize(content);
 
   res.status(200).json({ mdxSource, code: 200 });
