2b586b09c2738c24b216cc0466a01d3b346eb7ad
0xhardman <1142783160@qq.com>
2023-05-24 10:00:12 +0800

    fix: img component using, beautify zksyncswap

--- components/ZksyncSwap.js
+++ components/ZksyncSwap.js
@@ -2,7 +2,7 @@ import { Box } from '@mui/material';
 
 export default function ZksyncSwap() {
   return (
-    <Box display="flex" justifyContent="center" marginTop={2} sx={{ width: '320px', margin: 'auto' }}>
+    <Box display="flex" justifyContent="center" marginTop={2} sx={{ width:{xs:'320px',md:'420px'}, margin: 'auto' }}>
       <iframe style={{ border: 'none' }} width="390" height="750" src="https://mfl2-zk-proxy.myfirst.io" />
     </Box>
   );
