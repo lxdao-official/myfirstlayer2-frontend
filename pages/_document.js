3b1a9cdd0faa4ac783fba345d73a920531bf0174
Bruce Xu <brucex2710@gmail.com>
2023-06-30 21:07:39 +0700

    Add ga and title

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -7,7 +8,7 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link rel="icon" href="/icons/favicon.png" />
-        <title>My First Layer2</title>
+        <title>My First Layer2 - An education project for newbies to learn and interact with Layer2</title>
         <meta
           name="description"
           content="MyFirstLayer2 is an education project for newbies to learn and interact with Layer2. As you can see from the name, our users are beginners curious about the knowledge and projects on Layer2. We will use funny and easy-to-understand diagrams and animation to explain Layer2 to newbies. "
@@ -23,6 +24,17 @@ export default function Document() {
           }
         />
         <meta property="og:url" content="https://lxdao.io/" />
+
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-YBM7P1VZH9" />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-YBM7P1VZH9');
+          `}
+        </Script>
       </Head>
       <base target="_blank"></base>
       <body>
